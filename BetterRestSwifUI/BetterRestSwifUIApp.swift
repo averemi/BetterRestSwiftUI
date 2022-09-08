ecd88c4e2ff0ec55c26f05acc3e537356afd846b
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-08 19:54:55 +0200

    Initial project creation.

---
+++ BetterRestSwifUI/BetterRestSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestSwifUIApp.swift
+//  BetterRestSwifUI
+//
+//  Created by Anastasiia Veremiichyk on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
