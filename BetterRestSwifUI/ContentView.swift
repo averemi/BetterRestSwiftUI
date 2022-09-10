5018b818827b73a6f16f7fd0182b820e034643b8
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-11 00:09:02 +0200

    Made some more UI changes.

--- BetterRestSwifUI/ContentView.swift
+++ BetterRestSwifUI/ContentView.swift
@@ -27,25 +27,22 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-        
+                Section("Desired amount of sleep") {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 0...20)
+                
+                Section("Daily coffee intake") {
+//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 0...20)
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(0..<21) { cup in
+                            Text(cup == 1 ? "1 cup" : "\(cup) cups")
+                        }
+                    }
                 }
             }
             .navigationTitle("BetterRest")
